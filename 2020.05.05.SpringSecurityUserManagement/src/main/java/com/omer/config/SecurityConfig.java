dd6d9122dce79b7e67e93e57782decc836e8e449
ceren <ceren@DESKTOP-O50U1A4>
2020-05-19 19:44:26 +0300

    first project

---
+++ 2020.05.05.SpringSecurityUserManagement/src/main/java/com/omer/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.omer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication()
+		.withUser(users.username("omer").password("omer").roles("ADMIN","PERSONEL"))
+		.withUser(users.username("ali").password("can").roles("PERSONEL"))
+		.withUser(users.username("okan").password("yilmaz").roles("PERSONEL"));
+		
+		// TODO Auto-generated method stub
+		//super.configure(auth);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http
+		  .authorizeRequests()
+		      .anyRequest()
+		      .authenticated();
+				
+		http
+		  .formLogin()
+		  		.loginPage("/loginForm")
+		  		.loginProcessingUrl("/authenticateTheUser")
+		  		.permitAll();
+		
+		http.logout().permitAll();
+
+	}
+	
+	
+	
+
+	
+}
